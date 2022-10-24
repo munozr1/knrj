bd996208b2706e03bcbfad4cf9be5027443603f7
nicholasmoreland <nmoreland18@outlook.com>
2022-10-23 20:39:02 -0500

    update: spotify auth implementation

--- components/SpotifyLogin.js
+++ components/SpotifyLogin.js
@@ -1,21 +1,48 @@
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import { ResponseType, useAuthRequest } from 'expo-auth-session';
+import { useEffect, useState } from 'react';
+
+const discovery = {
+  authorizationEndpoint: 'https://accounts.spotify.com/authorize',
+  tokenEndpoint: 'https://accounts.spotify.com/api/token',
+};
 
 const spotifylogin = (props) => {
-    return (
-        <View style={[styles.container, styles.shadow]}>
-            {/* <View >
-                <Text style={[styles.label]} >{props.topLabel}</Text>
-            </View> */}
-            {/* <View>
-            </View> */}
-            <TouchableOpacity style={{
-              // borderWidth: 1,
-              width: 170
-            }}>
-                <Text style={styles.label}>{props.label}</Text>
-            </TouchableOpacity>
-        </View>
-    )
+  const [token, setToken] = useState("");
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      responseType: ResponseType.Token,
+      clientId: '67e4ad90bc6e4bec9eb4e4648117d547',
+      scopes: [
+        'user-read-currently-playing',
+        'user-read-recently-played',
+        'user-read-playback-state',
+        'user-top-read',
+        'user-modify-playback-state',
+        'streaming',
+        'user-read-email',
+        'user-read-private',
+      ],
+      usePKCE: false,
+      redirectUri: 'exp://192.168.1.146:19000'
+    },
+    discovery
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { access_token } = response.params;
+      setToken(access_token);
+    }
+  }, [response]);
+
+  return (
+    <View style={[styles.container, styles.shadow]}>
+        <TouchableOpacity style={{ width: 170 }} onPress={() => { promptAsync(); }}>
+          <Text style={styles.label}>{props.label}</Text>
+        </TouchableOpacity>
+    </View>
+  )
 }
 
 let styles = StyleSheet.create({
@@ -34,8 +61,7 @@ let styles = StyleSheet.create({
         shadowOpacity: 1/3,
         elevation: 1,
         backgroundColor: "white", // invisible color
-      }
-      ,
+      },
       container:{
         backgroundColor: "white",
         borderRadius: 37,
